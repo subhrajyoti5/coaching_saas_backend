875d93cd2a1a3553bae6d0fe019a7dd6d99410b6
Subhrajyoti Sahoo <subhrajyotisahoo08@gmail.com>
2026-04-14 16:55:04 +0530

    feat: add RevenueCat configuration and endpoint for retrieving API key

--- routes/subscriptionRoutes.js
+++ routes/subscriptionRoutes.js
@@ -4,13 +4,15 @@ const {
   createSubscription,
   getMySubscription,
   cancelSubscription,
-  getEntitlementStatus
+  getEntitlementStatus,
+  getRevenueCatConfig
 } = require('../controllers/subscriptionController');
 const { authenticateToken } = require('../middleware/auth');
 const { ownerOnly } = require('../middleware/roles');
 
 const router = express.Router();
 
+router.get('/config', getRevenueCatConfig);
 router.post('/create', authenticateToken, ownerOnly, createSubscription);
 router.get('/me', authenticateToken, ownerOnly, getMySubscription);
 router.post('/cancel', authenticateToken, ownerOnly, cancelSubscription);
