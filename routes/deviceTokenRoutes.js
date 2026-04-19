019f14bfca3414ec6e662e71daa71526090d5764
Subhrajyoti Sahoo <subhrajyotisahoo08@gmail.com>
2026-04-19 18:41:17 +0530

    feat: add diagnostics route for device token management

--- routes/deviceTokenRoutes.js
+++ routes/deviceTokenRoutes.js
@@ -3,11 +3,14 @@ const router = express.Router();
 
 const {
   registerDeviceToken,
-  deactivateDeviceToken
+  deactivateDeviceToken,
+  getNotificationDiagnostics
 } = require('../controllers/deviceTokenController');
 const { authenticateToken } = require('../middleware/auth');
+const { ownerOnly } = require('../middleware/roles');
 
 router.post('/register', authenticateToken, registerDeviceToken);
 router.post('/deactivate', authenticateToken, deactivateDeviceToken);
+router.get('/diagnostics', authenticateToken, ownerOnly, getNotificationDiagnostics);
 
 module.exports = router;
