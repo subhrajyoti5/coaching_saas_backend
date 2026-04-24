7b3dcad74e7ae86e12cbeecffc1dc4f6103ac45a
Subhrajyoti Sahoo <subhrajyotisahoo08@gmail.com>
2026-04-24 12:28:21 +0530

    f

--- routes/documentRoutes.js
+++ routes/documentRoutes.js
@@ -10,19 +10,19 @@ const {
   previewDocument
 } = require('../controllers/documentController');
 const { authenticateToken } = require('../middleware/auth');
-const { teacherOnly, studentOnly } = require('../middleware/roles');
+const { teacherOrOwner, studentOnly } = require('../middleware/roles');
 const { uploadTeacherDocument } = require('../middleware/upload');
 const {
   validateUploadTeacherDocument,
   validateUpdateTeacherDocument
 } = require('../middleware/validation');
 
-router.post('/upload', authenticateToken, teacherOnly, uploadTeacherDocument.single('file'), validateUploadTeacherDocument, uploadDocument);
-router.get('/my-documents', authenticateToken, teacherOnly, getMyDocuments);
+router.post('/upload', authenticateToken, teacherOrOwner, uploadTeacherDocument.single('file'), validateUploadTeacherDocument, uploadDocument);
+router.get('/my-documents', authenticateToken, teacherOrOwner, getMyDocuments);
 router.get('/student-feed', authenticateToken, studentOnly, getStudentFeed);
 router.get('/preview/:token', previewDocument);
 router.get('/:documentId/preview-url', authenticateToken, getPreviewUrl);
-router.put('/:documentId', authenticateToken, teacherOnly, validateUpdateTeacherDocument, updateDocument);
-router.delete('/:documentId', authenticateToken, teacherOnly, deleteDocument);
+router.put('/:documentId', authenticateToken, teacherOrOwner, validateUpdateTeacherDocument, updateDocument);
+router.delete('/:documentId', authenticateToken, teacherOrOwner, deleteDocument);
 
 module.exports = router;
